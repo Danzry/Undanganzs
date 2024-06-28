f8491ce23e911514c6eeb01987e0844a002a8183
Danz <danidarwisy0@gmail.com>
2024-06-28 09:49:27 +0800

    add twilio whatsapp APi

--- app/Livewire/BulkInviteModal.php
+++ app/Livewire/BulkInviteModal.php
@@ -5,6 +5,8 @@ namespace App\Livewire;
 use App\Models\FootballGuest;
 use LivewireUI\Modal\ModalComponent;
 use TallStackUi\Traits\Interactions;
+use Twilio\Rest\Client;
+use Exception;
 
 class BulkInviteModal extends ModalComponent
 {
@@ -22,19 +24,38 @@ class BulkInviteModal extends ModalComponent
     public function invite()
     {
 
-        dd($this->selectedGuests);
-        // foreach ($this->selectedGuests as $id) {
-        //     $selectedGuests = FootballGuest::findOrFail($id);
+        $sid = config('services.twilio.sid');
+        $token = config('services.twilio.token');
+        $from = config('services.twilio.whatsapp_from');
+        $message = "test";
 
+        foreach ($this->selectedGuests as $id) {
+            $to = FootballGuest::findOrFail($id);
 
-        //     if ($selectedGuests) {
-                
-        //     } else {
-        //         session()->flash('error', 'Guest not found.');
-        //     }
-        // }
+            if ($to && $to->phone_number) {
+               
+                try {
+                    $twilio = new Client($sid, $token);
 
+                    $res = $twilio->messages->create(
+                        'whatsapp:' . $to->phone_number,
+                        // 'whatsapp:+601117871890',
+                        array(
+                            "from" => 'whatsapp:' . $from,
+                            "body" => $message
+                        )
+                    );
+                    // dd($res);
+                } catch (Exception $e) {
+                    return back()->with(['error' => $e->getMessage()]);
+                }
 
+            } else {
+                session()->flash('error', 'Guest not found.');
+            }
+           
+        }
+        $this->closeModal();
     }
 
     public function render()
